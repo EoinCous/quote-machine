9dedbd3c957c20d640cc30bc05272eab17f77c89
EoinCous <eoin.cousins@gmail.com>
2025-04-25 17:01:49 +1000

    basic single quote displayed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div className="App-header" id="quote-box">
+      <p id="text">What doesn't kill you makes you stronger</p>
+      <p id="author">Fredrick Nietsche</p>
+      <button id="new-quote">Click for a new quote</button>
+      <a 
+        id="new-quote" 
+        className="App-link"
+        href="https://example.com" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        Share on twitter
+      </a>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+// Import your individual reducers here
+// import messagesReducer from './messagesReducer';
+
+const rootReducer = combineReducers({
+  // messages: messagesReducer
+});
+
+export default rootReducer;
